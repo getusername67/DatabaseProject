d02eaf037285408abc2154a077a9e96f82f79762
getusername67 <61632919+getusername67@users.noreply.github.com>
2022-04-15 22:05:22 -0400

    univProf html fix and js

---
+++ univProf.js
@@ -0,0 +1,13 @@
+var regUniv = document.querySelector(".profileSubmit");
+var numStudents;
+var univName;
+var univDesc;
+var address;
+
+regUniv.addEventListener('click', e => {
+    numStudents = document.getElementById('numStudents').value;
+    univName = document.getElementById('univName').value;
+    univDesc = document.getElementById('univDesc').value;
+    address = document.getElementById('address').value;
+    
+})
