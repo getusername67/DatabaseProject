f22d617f935f6c7a3f9a077eb414329528cc2c7d
getusername67 <61632919+getusername67@users.noreply.github.com>
2022-04-15 20:47:01 -0400

    rso

--- rso.js
+++ rso.js
@@ -1,5 +1,17 @@
-document.getElementById("myButton").onclick = function(){
 
-    var RSOName = document.getElementById("RSOName");
-    console.log(RSOName);
-}
+var createRSO = document.querySelector(".RSOsubmit");
+var RSOname = '';
+var AdminName ='';
+var lines = [];
+var names = [];
+createRSO.addEventListener('click', e => {
+    RSOname = document.getElementById('RSOName').value;
+    AdminName = document.getElementById('adminName').value;
+    lines = $('listMembers').val().split('\n');
+    for(var i = 0;i < lines.length;i++){
+        names[i] = lines[i];
+    }
+    if(lines.length < 4){
+        alert("Too few members to create an RSO")
+    }
+})
