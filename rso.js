64bbc5f6576619bb6d4d48869afdcba151d32a44
getusername67 <61632919+getusername67@users.noreply.github.com>
2022-04-15 21:19:21 -0400

    rso

--- rso.js
+++ rso.js
@@ -2,16 +2,46 @@
 var createRSO = document.querySelector(".RSOsubmit");
 var RSOname = '';
 var AdminName ='';
-var lines = [];
-var names = [];
 createRSO.addEventListener('click', e => {
     RSOname = document.getElementById('RSOName').value;
     AdminName = document.getElementById('adminName').value;
-    lines = $('listMembers').val().split('\n');
-    for(var i = 0;i < lines.length;i++){
-        names[i] = lines[i];
-    }
-    if(lines.length < 4){
-        alert("Too few members to create an RSO")
-    }
+    var Fname1 = document.getElementById('Fname1').value;
+    var Lname1 = document.getElementById('Lname1').value;
+    var uId1 = document.getElementById('uId1').value;
+
+    var Fname2 = document.getElementById('Fname2').value;
+    var Lname2 = document.getElementById('Lname2').value;
+    var uId2 = document.getElementById('uId2').value;
+
+    var Fname3 = document.getElementById('Fname3').value;
+    var Lname3 = document.getElementById('Lname3').value;
+    var uId3 = document.getElementById('uId3').value;
+
+    var Fname4 = document.getElementById('Fname4').value;
+    var Lname4 = document.getElementById('Lname4').value;
+    var uId4 = document.getElementById('uId4').value;
+
+    var Fname5 = document.getElementById('Fname5').value;
+    var Lname5 = document.getElementById('Lname5').value;
+    var uId5 = document.getElementById('uId5').value;
+
+    var Fname6 = document.getElementById('Fname6').value;
+    var Lname6 = document.getElementById('Lname6').value;
+    var uId6 = document.getElementById('uId6').value;
+
+    var Fname7 = document.getElementById('Fname7').value;
+    var Lname7 = document.getElementById('Lname7').value;
+    var uId7 = document.getElementById('uId7').value;
+
+    var Fname8 = document.getElementById('Fname8').value;
+    var Lname8 = document.getElementById('Lname8').value;
+    var uId8 = document.getElementById('uId8').value;
+
+    var Fname9 = document.getElementById('Fname9').value;
+    var Lname9 = document.getElementById('Lname9').value;
+    var uId9 = document.getElementById('uId9').value;
+
+    var Fname10 = document.getElementById('Fname10').value;
+    var Lname10 = document.getElementById('Lname10').value;
+    var uId10 = document.getElementById('uId10').value;
 })
