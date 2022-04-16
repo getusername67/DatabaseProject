106b4a4b42cab1f915d24921d42d755856e68918
getusername67 <61632919+getusername67@users.noreply.github.com>
2022-04-15 22:18:02 -0400

    comment

---
+++ comment.js
@@ -0,0 +1,7 @@
+var comment = document.querySelector(".commentSubmit");
+var commentDesc;
+
+getUser.addEventListener('click', e => {
+    commentDesc = document.getElementById('comment').value;
+    
+})
